411787719f13f2ce3074b0161faba1f6613d8da5
Amit Anand <amitanand3@outlook.com>
2020-04-26 23:59:43 +0530

    rimraf

--- client/config/api.js
+++ client/config/api.js
@@ -1,3 +1,3 @@
 export const apiServiceUrl = process.env.DEV
   ? "http://localhost:3000/"
-  : "https://save-my-sinking-boat.web.app/"; //production server url
+  : "https://save-my-sinking-boat.herokuapp.com/"; //production server url
