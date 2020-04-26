be754dea1e55760d3c5fd566e7515b26a4f6fff7
Amit Anand <amitanand3@outlook.com>
2020-04-27 01:57:27 +0530

    eslint fix

--- webpack/base.js
+++ webpack/base.js
@@ -1,27 +1,27 @@
-'use strict'
-let path = require('path')
-let defaultSettings = require('./defaults')
+"use strict"
+let path = require("path")
+let defaultSettings = require("./defaults")
 
 module.exports = {
   port: defaultSettings.port,
   debug: true,
-  devtool: 'eval',
+  devtool: "eval",
   output: {
-    path: path.join(__dirname, '/../public'),
-    filename: 'bundle.js',
+    path: path.join(__dirname, "/../public"),
+    filename: "bundle.js",
     publicPath: `.${defaultSettings.publicPath}`
   },
   resolve: {
     modulesDirectories: [
-      'client',
-      'node_modules'
+      "client",
+      "node_modules"
     ],
-    extensions: ['', '.js', '.jsx']
+    extensions: ["", ".js", ".jsx"]
   },
   postcss: function () {
     return [
-      require('precss'),
-      require('autoprefixer')
+      require("precss"),
+      require("autoprefixer")
     ]
   }
 }
--- webpack/dist.js
+++ webpack/dist.js
@@ -1,26 +1,26 @@
-'use strict'
+"use strict"
 
-let path = require('path')
-let webpack = require('webpack')
+let path = require("path")
+let webpack = require("webpack")
 
-let baseConfig = require('./base')
-let defaultSettings = require('./defaults')
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+let baseConfig = require("./base")
+let defaultSettings = require("./defaults")
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 let config = Object.assign({}, baseConfig, {
-  entry: path.join(__dirname, '../client/app.js'),
+  entry: path.join(__dirname, "../client/app.js"),
   cache: false,
-  devtool: 'null',
+  devtool: "null",
   plugins: [
     new webpack.optimize.DedupePlugin(),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"production"',
-      'process.env.DEV': process.env.DEV || false
+      "process.env.NODE_ENV": "\"production\"",
+      "process.env.DEV": process.env.DEV || false
     }),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.AggressiveMergingPlugin(),
     new webpack.NoErrorsPlugin(),
-    new ExtractTextPlugin('./css/style.css')
+    new ExtractTextPlugin("./css/style.css")
   ],
   module: defaultSettings.getDefaultModules()
 })
