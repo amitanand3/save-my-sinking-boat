6075801e1d0100e914226a28290e76dde733fa76
Amit Anand <amitanand3@outlook.com>
2020-04-27 01:28:33 +0530

    heroku changes

--- client/server.js
+++ client/server.js
@@ -2,13 +2,13 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-
+const port = process.env.PORT || 4000;
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("*", function (request, response) {
   response.sendFile(path.resolve(__dirname, "../public", "index.html"));
 });
 
-app.listen(4000);
+app.listen(port);
 
-console.log("Server running on http://localhost:4000");
+console.log("Server running on http://localhost:"+port);
